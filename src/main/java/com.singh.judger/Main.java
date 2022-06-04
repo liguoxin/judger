bf3fc66da99a57d61fc7f94e2ec57da9f174851d
LIGUOXIN <woliguoxin@sina.com>
2022-06-04 15:28:43 +0800

    init

---
+++ src/main/java/com.singh.judger/Main.java
@@ -0,0 +1,13 @@
+package com.singh.judger;
+
+/**
+ * @author liguoxin
+ * @email guoxin.li@xintech.cn
+ * @date 15:23 2022/6/4
+ * @Copyright Copyright(c) 2021 xintech Inc.All Rights Reserved.
+ * @desc
+ */
+public class Main {
+
+
+}
